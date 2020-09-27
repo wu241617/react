0d9ba6d422021167dedd58966c81c33cd835ddbb
吴浩 <wuhao@bonree.com>
2020-09-27 18:41:27 +0800

    feta:新增

--- 04_webpack4/src/index.js
+++ 04_webpack4/src/index.js
@@ -1,2 +1,3 @@
+
 // 假设 main.js 还是我们的入口文件
 console.log('ok');
--- 05_BaseUse_react/webpack.config.js
+++ 05_BaseUse_react/webpack.config.js
@@ -1,10 +1,11 @@
+
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');//导入在内存中自动生成index页面的插件
 
 //创建一个插件的实例对象
 const htmlPlugin = new  HtmlWebPackPlugin({
 	template: path.join(__dirname, './src/index.html'),//源文件
-	filename:: 'index.html' //生成的内存中首页的名称
+	filename: 'index.html' //生成的内存中首页的名称
 })
 
 // 向外暴露一个打包的配置对象,webpack基于node构建的，所以支持所有Node API和语法
