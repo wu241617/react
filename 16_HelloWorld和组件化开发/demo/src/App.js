454f12d38f805093ef6586969f3d02ed921c7cc0
吴浩 <wuhao@bonree.com>
2020-11-03 18:39:57 +0800

    feat:新增代码

---
+++ 16_HelloWorld和组件化开发/demo/src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+// import React from 'react'
+// const Component = React.Component
+
+class App extends Component {
+    render(){
+        return (
+            <div>
+                HELLO_WORLD  !!!
+            </div>
+        )
+    }
+}
+
+export default App
